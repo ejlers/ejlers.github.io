0ef98ff5c36dada5b7f33ad7bb350fff2699a625
Carl Ejlers <ejlers@me.com>
2016-11-22 00:11:39 +0000

    to stacey

---
+++ js/tostacey.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    $("button").click(function(){
+    	$(".background-img").css({"background-image" : "url('https://media.giphy.com/media/hbgGNb6Pa64bS/giphy.gif')"});
+		$("button").hide();
+		$(".stacey-text").fadeIn(2000);
+	});
+});
