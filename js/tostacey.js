563de16a196f547bb04f8e872b6eb90e4cff053e
Carl Ejlers <ejlers@me.com>
2016-11-22 00:19:35 +0000

    stacey tweaks

--- js/tostacey.js
+++ js/tostacey.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     $("button").click(function(){
-    	$(".background-img").css({"background-image" : "url('https://media.giphy.com/media/hbgGNb6Pa64bS/giphy.gif')"});
+    	$(".background-img").css({"background-image" : "url('https://media.giphy.com/media/hbgGNb6Pa64bS/giphy.gif')", "opacity": "0.8"});
 		$("button").hide();
 		$(".stacey-text").fadeIn(2000);
 	});
