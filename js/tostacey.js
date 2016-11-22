c849048b17f37820bae7c3fd9b2024cdfc663cdb
Carl Ejlers <ejlers@me.com>
2016-11-22 16:57:08 +0000

    day 2

--- js/tostacey.js
+++ js/tostacey.js
@@ -1,8 +1,9 @@
 
 $(document).ready(function(){
     $("button").click(function(){
-    	$(".background-img").css({"background-image" : "url('https://media.giphy.com/media/hbgGNb6Pa64bS/giphy.gif')", "opacity": "0.8"});
+    	$(".background-img").css({"background-image" : "url('https://media.giphy.com/media/l0HlScP4Pl31ADK8M/giphy.gif')"});
 		$("button").hide();
 		$(".stacey-text").fadeIn(2000);
+		$(".stacey-img").fadeIn(2000);
 	});
 });
