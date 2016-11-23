fe1467a9a50bb1b4ceb9e251bb92c55de7ac81b8
Carl Ejlers <ejlers@me.com>
2016-11-23 12:22:05 +0000

    delete

--- js/tostacey.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-    $("button").click(function(){
-    	$(".background-img").css({"background-image" : "url('https://media.giphy.com/media/l0HlScP4Pl31ADK8M/giphy.gif')"});
-		$("button").hide();
-		$(".stacey-text").fadeIn(2000);
-		$(".stacey-img").fadeIn(2000);
-	});
-}); 
