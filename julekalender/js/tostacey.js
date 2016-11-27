d20282edafb387bb7cd93fc2d01dd99f29651c17
Carl Ejlers <ejlers@me.com>
2016-11-27 17:41:33 +0100

    6

--- julekalender/js/tostacey.js
+++ julekalender/js/tostacey.js
@@ -1,7 +1,16 @@
 
 $(document).ready(function(){
-    $("button").click(function(){
+  $("button").click(function(){
 		$(".start-screen").hide();
 		$(".content").fadeIn(2000);
 	});
+}); 
+
+$(document).ready(function(){
+		$(".forward").click(function(){
+			$("a").attr("href", "http://www.google.com/")
+		});
+		$(".back").click(function(){
+			$("a").attr("href", "http://www.google.com/")
+		});
 }); 
